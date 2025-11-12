60eef8ee2b52e7ba5c1f7706af86213822c26e58
unknown <dkuz6323@gmail.com>
2025-11-12 21:14:47 +0200

    completed tasks

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,6 +8,27 @@
  */
 function transformStateWithClones(state, actions) {
   // write code here
+  let currentState = { ...state };
+  const result = [];
+
+  for (const key of actions) {
+    let nextState = { ...currentState };
+
+    if (key.type === 'clear') {
+      nextState = {};
+    } else if (key.type === 'addProperties') {
+      nextState = { ...nextState, ...key.extraData };
+    } else if (key.type === 'removeProperties') {
+      for (const item of key.keysToRemove) {
+        delete nextState[item];
+      }
+    }
+
+    result.push(nextState);
+    currentState = nextState;
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
